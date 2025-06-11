d421620a0013254378b3b6f2b88391b59af2a511
jihyun <jihyun3066@naver.com>
2025-06-11 14:03:48 +0900

    [ 프로그래머스 입문 ] 배열 두배 만들기

---
+++ src/main/java/pra/lv0/입문/배열두배만들기.java
@@ -0,0 +1,19 @@
+package pra.lv0.입문;
+
+import java.util.Arrays;
+public class 배열두배만들기 {
+
+    public static int[] solution(int [] data){
+        return Arrays.stream(data).map(
+               i->  i*2
+        ).toArray();
+    }
+
+    public int[] solution2(int[] numbers) {
+
+        for(int i=0;i<numbers.length;i++){
+            numbers[i] = numbers[i] * 2;
+        }
+        return numbers;
+    }
+}
