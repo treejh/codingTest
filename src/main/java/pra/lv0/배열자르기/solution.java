a3eee2b55d9bb0f93e68ff3492fc443db714f389
jihyun <jihyun3066@naver.com>
2025-06-14 23:45:43 +0900

    [ 프로그래머스 입문 ] : 배열 자르기

---
+++ src/main/java/pra/lv0/배열자르기/solution.java
@@ -0,0 +1,11 @@
+package pra.lv0.배열자르기;
+
+import java.util.Arrays;
+
+public class solution {
+
+    public int[] solution(int[] numbers, int num1, int num2) {
+
+        return Arrays.copyOfRange(numbers,num1,num2+1);
+    }
+}
