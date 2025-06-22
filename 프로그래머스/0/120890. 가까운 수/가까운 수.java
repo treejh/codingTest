7a84cea5d4cc49eb163db7a6e5316c1974d6bc06
jihyun <jihyun3066@naver.com>
2025-06-22 14:46:34 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer =  Integer.MAX_VALUE;;
+        int result =0; 
+        
+        for(int i=0;i<array.length;i++){
+            int compare = Math.abs(n-array[i]);
+            
+            if(compare < answer){
+                answer = compare;
+                result = array[i];
+            }else if(compare == answer){
+                if(result >= array[i]){
+                    result = array[i];
+                }
+            }
+        }
+        return result;
+    }
+}
