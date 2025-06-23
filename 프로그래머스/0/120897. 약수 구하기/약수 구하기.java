d717b6e68f034d0662728621c7599af61702a694
jihyun <jihyun3066@naver.com>
2025-06-23 11:25:39 +0900

    [level 0] Title: 약수 구하기, Time: 3.52 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 class Solution {
     public int[] solution(int n) {
         ArrayList<Integer> result = new ArrayList<>();
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 result.add(i);
+                  if (i != n / i) {
+                    result.add(n / i);
+                }
             }
         }
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
