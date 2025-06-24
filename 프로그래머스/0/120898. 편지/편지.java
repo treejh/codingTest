d8eeeba81a4720e5db8c23f67d2f0cbd3c1a8566
jihyun <jihyun3066@naver.com>
2025-06-24 20:42:12 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String message) {
-        int answer = 0;
-        for(int i=0;i<message.length();i++){
-            answer+=2;
-        }
-        return answer;
+        
+        return message.length()*2;
     }
 }
