bb6a0f5c9100f6082002c9e1ffde7b7ac075e20d
jihyun <jihyun3066@naver.com>
2025-07-01 23:12:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        long answer = num;
+        for(int i=0;i<500;i++){
+            if(answer==1){
+                return i;
+            }
+            else if(answer%2==0){
+                answer /=2;
+            }else{
+                answer = (answer * 3)+1;
+            }
+        }
+        return -1;
+    }
+}
