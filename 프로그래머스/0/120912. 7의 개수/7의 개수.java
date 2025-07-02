7c438c65162878db9084117f99a69b193d0e7250
jihyun <jihyun3066@naver.com>
2025-07-02 14:29:53 +0900

    [level 0] Title: 7의 개수, Time: 0.03 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,15 +1,14 @@
+
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        for(int i=0;i<array.length;i++){
-            String [] data = String.valueOf(array[i]).split("");
-            
-            for(int j=0;j<data.length;j++){
-                if(data[j].equals("7")){
+        for(int a : array){
+            while(a != 0){
+                if(a % 10 == 7){
                     answer++;
                 }
+                a /= 10;
             }
-            
         }
         return answer;
     }
