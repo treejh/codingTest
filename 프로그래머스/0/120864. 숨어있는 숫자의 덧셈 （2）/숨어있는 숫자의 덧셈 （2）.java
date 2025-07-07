e856a1bca215d1af3ab57bde9c4e6ff227190169
jihyun <jihyun3066@naver.com>
2025-07-07 14:40:16 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.77 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String onlyNumbers = my_string.replaceAll("[^0-9]"," ");
+        String [] numbersList = onlyNumbers.trim().split("\\s+");
+
+        for(String number : numbersList ){
+            if (!number.isEmpty()) {  // 빈 문자열이면 건너뜀
+            answer += Integer.parseInt(number);
+    }
+        }
+        return answer;
+    }
+}
