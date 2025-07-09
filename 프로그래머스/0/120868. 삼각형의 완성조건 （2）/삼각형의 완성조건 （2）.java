5c0c3156f8cf8b23f3069c837e14592089bf68e7
jihyun <jihyun3066@naver.com>
2025-07-09 15:26:41 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.05 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -4,18 +4,10 @@ class Solution {
     public int solution(int[] sides) {
         int maxValue = Math.max(sides[0], sides[1]);
         int minValue = Math.min(sides[0], sides[1]);
-        Set<Integer> data = new HashSet<>();
-
-        // 가장 긴 변이 sides의 두 수 중 하나일 때 가능한 나머지 한 변
-        for (int i = maxValue - minValue + 1; i <= maxValue; i++) {
-            data.add(i);
-        }
-
-        // 나머지 한 변이 가장 긴 변일 때 가능한 경우
-        for (int j = maxValue + 1; j < maxValue + minValue; j++) {
-            data.add(j);
-        }
-
-        return data.size();
+        
+        int minus = maxValue-minValue;
+        int plus = maxValue+minValue;
+    
+        return plus-minus-1;
     }
 }
