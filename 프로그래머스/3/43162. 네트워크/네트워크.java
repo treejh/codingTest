9d760df7346d614f39e61ae3a224eb8f48c479ae
jihyun <jihyun3066@naver.com>
2025-07-21 15:10:57 +0900

    [level 3] Title: 네트워크, Time: 0.43 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,27 +1,41 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
 class Solution {
+    static boolean visit[];
+    static Queue<Integer> que = new LinkedList<>();
+    
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[n]; //false
+        visit = new boolean[n];
         
         for(int i=0;i<n;i++){
-            if(!visited[i]){ //false 이면 방문 안한거임
-                bfs(i,computers,visited);
-                answer++; 
+            if(visit[i]==false){
+                bfs(i,computers,n);
+                answer++;
             }
         }
+        
         
         return answer;
     }
     
-    public void bfs(int current, int [][] computers, boolean [] visited){
+    
+    public void bfs(int i, int computers [][], int n){
+        que.offer(i);
+        visit[i]=true;
         
-        visited[current]=true;
-        
-        for(int i=0;i<computers.length;i++){
-            if(computers[current][i] == 1 && !visited[i]){
-                bfs(i,computers,visited);
+        //큐가 비어있지 않을때까지  -> 큐에 값이 있을때만 
+        while(!que.isEmpty()){
+            int value = que.poll();
+            
+            for(int j=0;j<n;j++){
+                if(!visit[j] && computers[value][j]==1){
+                    visit[j] = true;
+                    que.offer(j);
+                }
             }
         }
-        
     }
 }
