7ced58bc988276a9e381121d92c19f9e4b149f2d
jihyun <jihyun3066@naver.com>
2025-07-28 15:48:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.66 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,42 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] maps) {
+        Queue<int [] > queue = new LinkedList<>();
         int n = maps.length;
         int m = maps[0].length;
+        
+        int nx[] = {1,-1,0,0};
+        int ny[] = {0,0,1,-1};
+        
         boolean[][] visited = new boolean[n][m];
-        
-        int [] dx ={0,0,1,-1};
-        int [] dy = {1,-1,0,0};
-        
-        Queue<int[]> queue = new LinkedList<>();
         queue.add(new int []{0,0,1});
-        visited[0][0] = true;
+        visited[0][0]=true;
         
         while(!queue.isEmpty()){
-            int [] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int count = cur[2];
+            int[] data = queue.poll();
+            int x = data[0];
+            int y = data[1];
+            int count = data[2];
             
-            if(x == n-1 && y == m-1){
-                return count ; 
+            if(x == n-1 && y == m-1 ){
+                return count;
             }
             
             for(int i=0;i<4;i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if( nx >= 0 && ny>=0 && nx < n 
-                  && ny < m){
-                     if (!visited[nx][ny] && maps[nx][ny] == 1) {
-                        visited[nx][ny] = true;
-                        queue.add(new int[]{nx, ny, count + 1});
-                    }
-                }
+                int countx = x+nx[i];
+                int county= y + ny[i];
+                
+                 if(countx >=0 && county>=0 && countx < n && county < m ){
+                     if(maps[countx][county]==1 && !visited[countx][county]){
+                         visited[countx][county] = true;
+                        queue.add(new int []{countx,county,count+1});
+                     }
+                 }
+                
             }
-            
         }
+        
+    
         return -1;
     }
 }
