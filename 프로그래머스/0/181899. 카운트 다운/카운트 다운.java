336cd9322de000b3507023598ee375b9c1ab65dc
jihyun <jihyun3066@naver.com>
2025-08-21 20:53:22 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int length = start_num-end_num;
+        int[] answer = new int [length+1];
+        for(int i=0;i<=length;i++){
+            answer[i] = start_num-i;
+        }
+        return answer;
+    }
+}
