d8ad281b4ef23318c514b8e51ef686bbbe53e9c7
jihyun <jihyun3066@naver.com>
2025-09-01 14:48:19 +0900

    [level 2] Title: 피보나치 수, Time: 3.27 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,18 @@
 class Solution {
+
     public int solution(int n) {
-        long arr []  = new long [2];
-        arr[0] = 0;
-        arr[1] = 1;
-        long fibo =0;
-        for(int i=2;i<=n;i++){
-            fibo = (arr[0] + arr[1]) % 1234567;  
-            arr[0] = arr[1];
-            arr[1] = fibo;
+        int answer = 0;
+        long[] arr=new long[n+1];
+        
+        arr[0]=0L;
+        arr[1]=1L;
+
+        for(int i=2; i<=n; i++){
+            arr[i]=(arr[i-1]+arr[i-2])%1234567L;
         }
-        return (int)fibo;
+        
+        answer=(int)(arr[n]);
+        
+        return answer;
     }
 }
